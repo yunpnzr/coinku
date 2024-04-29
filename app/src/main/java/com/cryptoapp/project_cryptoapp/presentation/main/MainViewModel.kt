5969727143574c48955c41d058d989473876f433
Yunian Edwi Risnawa <yunianedwi01@gmail.com>
2024-04-29 20:53:49 +0700

    Revision base project

---
+++ app/src/main/java/com/cryptoapp/project_cryptoapp/presentation/main/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.cryptoapp.project_cryptoapp.presentation.main
+
+import androidx.lifecycle.ViewModel
+import com.cryptoapp.project_cryptoapp.data.repository.listcrypto.ListCryptoRepository
+
+class MainViewModel(
+    private val listCryptoRepository: ListCryptoRepository,
+) : ViewModel() {
+
+}
